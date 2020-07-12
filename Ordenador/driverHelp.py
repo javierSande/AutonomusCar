45ae23f604912a5f12c62e7a94beee0c684edcb3
Javier <javiersanderios@icloud.com>
2020-07-12 23:06:22 +0200

    Initial commit

---
+++ Ordenador/driverHelp.py
@@ -0,0 +1,115 @@
+import serial
+import cv2
+import math
+from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = PiGPIOFactory(host='192.168.1.69')
+right = LED(27, pin_factory=factory)
+left = LED(17, pin_factory=factory)
+forward = LED(10, pin_factory=factory)
+backward = LED(9, pin_factory=factory)
+
+
+class RCControl(object):
+
+    def __init__(self):
+        self = self
+
+    def steer(self, prediction):
+        if prediction == 2:
+            forward.on()
+            print("Forward")
+        elif prediction == 0:
+            forward.on()
+            left.on()
+            print("Left")
+        elif prediction == 1:
+            forward.on()
+            right.on()
+            print("Right")
+        else:
+            self.stop()
+
+    def stop(self):
+        forward.off()
+        backward.off()
+        right.off()
+        left.off()
+
+
+class DistanceToCamera(object):
+
+    def __init__(self):
+        # camera params
+        self.alpha = 8.0 * math.pi / 180    # degree measured manually
+        self.v0 = 119.865631204             # from camera matrix
+        self.ay = 332.262498472             # from camera matrix
+
+    def calculate(self, v, h, x_shift, image):
+        # compute and return the distance from the target point to the camera
+        d = h / math.tan(self.alpha + math.atan((v - self.v0) / self.ay))
+        if d > 0:
+            cv2.putText(image, "%.1fcm" % d,
+                        (image.shape[1] - x_shift, image.shape[0] - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+        return d
+
+
+class ObjectDetection(object):
+
+    def __init__(self):
+        self.red_light = False
+        self.green_light = False
+        self.yellow_light = False
+
+    def detect(self, cascade_classifier, gray_image, image):
+
+        # y camera coordinate of the target point 'P'
+        v = 0
+
+        # minimum value to proceed traffic light state validation
+        threshold = 150
+
+        # detection
+        cascade_obj = cascade_classifier.detectMultiScale(
+            gray_image,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30))
+
+        # draw a rectangle around the objects
+        for (x_pos, y_pos, width, height) in cascade_obj:
+            cv2.rectangle(image, (x_pos + 5, y_pos + 5), (x_pos + width - 5, y_pos + height - 5), (255, 255, 255), 2)
+            v = y_pos + height - 5
+            # print(x_pos+5, y_pos+5, x_pos+width-5, y_pos+height-5, width, height)
+
+            # stop sign
+            if width / height == 1:
+                cv2.putText(image, 'STOP', (x_pos, y_pos - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+            # traffic lights
+            else:
+                roi = gray_image[y_pos + 10:y_pos + height - 10, x_pos + 10:x_pos + width - 10]
+                mask = cv2.GaussianBlur(roi, (25, 25), 0)
+                (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask)
+
+                # check if light is on
+                if maxVal - minVal > threshold:
+                    cv2.circle(roi, maxLoc, 5, (255, 0, 0), 2)
+
+                    # Red light
+                    if 1.0 / 8 * (height - 30) < maxLoc[1] < 4.0 / 8 * (height - 30):
+                        cv2.putText(image, 'Red', (x_pos + 5, y_pos - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                        self.red_light = True
+
+                    # Green light
+                    elif 5.5 / 8 * (height - 30) < maxLoc[1] < height - 30:
+                        cv2.putText(image, 'Green', (x_pos + 5, y_pos - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0),2)
+                        self.green_light = True
+
+                    # yellow light
+                    # elif 4.0/8*(height-30) < maxLoc[1] < 5.5/8*(height-30):
+                    #    cv2.putText(image, 'Yellow', (x_pos+5, y_pos - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+                    #    self.yellow_light = True
+        return v
